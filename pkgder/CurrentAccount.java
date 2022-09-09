01525aba38887833fedd2e779ff5892e977184cc
Bannuru Veerendra <88670654+mr-veeru@users.noreply.github.com>
2022-09-09 09:03:43 +0530

    Update CurrentAccount.java

--- pkgder/CurrentAccount.java
+++ pkgder/CurrentAccount.java
@@ -1,20 +1,27 @@
 package pkgder;
+
 import java.util.Scanner;
 import pkgbase.Account;
+
 public class CurrentAccount extends Account
 {
-	static int accnumGen=50000;
+	static int accnumGen=65729;
+	static int pinGen=4321;
+	
 	public CurrentAccount()
 	{
-		rateOfInterest=0.0;
+		rateOfInterest="0%";
 	}
 	
 	public void createAccount()
 	{
 		Scanner sc= new Scanner(System.in);
 		accNumber=accnumGen++;
+		pin = pinGen++;
 		System.out.println("Enter the Account holder name");
 		accountHolderName=sc.nextLine();
+		System.out.println("Enter Your PAN card number");
+		PAN_number=sc.nextInt();
 		showAccountDetails();
 	}
 }
