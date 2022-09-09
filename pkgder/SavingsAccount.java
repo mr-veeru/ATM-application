031bdffee08e68062b94efc87ea5235c440b2d4f
Bannuru Veerendra <88670654+mr-veeru@users.noreply.github.com>
2022-09-09 09:04:24 +0530

    Update SavingsAccount.java

--- pkgder/SavingsAccount.java
+++ pkgder/SavingsAccount.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 import pkgbase.Account;
 public class SavingsAccount extends Account 
 {
-    static int accnumGen=10000;
+    static int accnumGen=25678;
+    static int pinGen=1234; 
+    
     public SavingsAccount()
     {
-        rateOfInterest=5.5;
+        rateOfInterest="4%";
     }
     
 	public void createAccount()
     {
         Scanner sc= new Scanner(System.in);
         accNumber = accnumGen++;
+        pin = pinGen++;
         System.out.println("Enter the Account holder name");
         accountHolderName=sc.nextLine();
         showAccountDetails();
