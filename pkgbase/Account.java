f874321033f29dd30e0e17a891a7d15ea8275559
Bannuru Veerendra <88670654+mr-veeru@users.noreply.github.com>
2022-09-09 09:02:56 +0530

    Update Account.java

--- pkgbase/Account.java
+++ pkgbase/Account.java
@@ -6,48 +6,52 @@ import pkgint.IAccount;
 public class Account implements IAccount 
 {
 	protected int accNumber;
-    protected String accountHolderName;
-    protected long accountBalance;
-    protected double rateOfInterest;
+	protected int pin;
+	protected String accountHolderName;
+	protected long accountBalance;
+	protected String rateOfInterest;
+	protected int PAN_number;
     
     Scanner sc=new Scanner(System.in);
     
     public void showAccountDetails()
     {
     	System.out.println("Account Number :"+accNumber);
+    	System.out.println("pin :"+ pin);
     	System.out.println("Account Holder name :"+accountHolderName);    
     	System.out.println("Account balance :"+accountBalance);
     	System.out.println("Rate of interest="+rateOfInterest);
     }
     
-    public void depositAmount(int accNumber) 
+    public void depositAmount(int accNumber, int pin) 
     {
-    	if(accNumber==this.accNumber)
+    	if(accNumber==this.accNumber && pin==this.pin)
     	{
     		System.out.println("Enter the amount to be deposited");
     		int amount=sc.nextInt();
     		accountBalance+=amount;
+    		System.out.println("Updated Balance in your account is "+accountBalance);
     	}
     }
     
-    public void balanceEnquiry(int accNumber) 
+    public void balanceEnquiry(int accNumber, int pin) 
     {
-        if(accNumber==this.accNumber)
+        if(accNumber==this.accNumber && pin==this.pin)
         {
         	System.out.println("Balance in account "+accountBalance);
         }
     }
 
-    public void withdrawAmount(int accNumber) 
+    public void withdrawAmount(int accNumber, int pin) 
     {
-        if(accNumber==this.accNumber)
+        if(accNumber==this.accNumber && pin==this.pin)
         {
-            System.out.print("Enter the amount to be withdraw ");    
+            System.out.println("Enter the amount to be withdraw ");    
             int amount=sc.nextInt();
             if(amount<=accountBalance)
             {
-                accountBalance=accountBalance-amount;
-                System.out.println("Balance in account "+accountBalance);    
+                accountBalance=accountBalance-amount; 
+                System.out.println("Updated Balance in your account is "+accountBalance);
             }
             else
             {
